ca23741d336bafa300a681d2a1dc69836aa6f977
isbrycee <isjinghao@gmail.com>
2025-10-20 09:17:05 +0800

    re-organize

--- tools/run_deepseek_distilled_llama3.3_70B.sh
+++
@@ -1,7 +0,0 @@
-vllm serve neuralmagic/DeepSeek-R1-Distill-Llama-70B-quantized.w4a16/ --tensor-parallel-size 1 --max-model-len 32768 \
-                                                        --enforce-eager \
-                                                        --gpu-memory-utilization 0.99 \
-                                                        --host 0.0.0.0 \
-                                                        --port 8080 \
-                                                        --served-model-name neuralmagic/DeepSeek-R1-Distill-Llama-70B-quantized.w4a16
-                                                        # --quantization gptq
